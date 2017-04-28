f8dc40f3c361f5306f79102b28d00369f57ad4a7
joasebastian <joa.pesonen@gmail.com>
2017-04-28 11:34:55 +0300

    noni
    jos nyt

--- Varastojarjestelma/src/varastojarjestelma/Varastojarjestelma.java
+++ Varastojarjestelma/src/varastojarjestelma/Varastojarjestelma.java
@@ -3,22 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package varastojarjestelma;
+import java.util.Scanner;
+
+
+
 
 /**
  *
- * @author user3
+ *
  */
 public class Varastojarjestelma {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    
+    public static Tuote testiTuote = new Tuote("123-456", "appelsiinit");
+    static Lajitteluhallinta lajittelu = new Lajitteluhallinta();
+    static Scanner lukija = new Scanner(System.in);
+    
+    //public static Paneeli paneeli = new Paneeli();
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        //jeejee
-        //tuoli
-        //Kulli
-    }  //pari muuta
+        int valinta = 0;
+        String testeri = testiTuote.getAvain();
+        System.out.println(testeri);
+        
+        /*vitun grafiikat
+        
+        Grafiikka grafiikka = new Grafiikka();
+        grafiikka.asetaNakyvyys();
+        */
+        do{
+            System.out.println("vaihtoehdot... mikä on valintasi?...");
+        }while(valinta!=0);
+        
+        
+        System.out.println(lajittelu.vapaatVarastopaikat());
+        
+        
+        
+        
+    }
+    
     
 }
